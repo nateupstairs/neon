6a8a6ad58b931b083effb2ebce32ef7b89542889
nateupstairs <nate.upstairs@gmail.com>
2026-04-20 17:15:08 -0700

    [scrap] additional functions

--- src/scrap_functions.cpp
+++ src/scrap_functions.cpp
@@ -646,6 +646,458 @@ S_nth(const vector<Node>& params, Scope* scope) {
 }
 
 json
+S_and(const vector<Node>& params, Scope* scope) {
+	for (const Node& param : params) {
+		json evaluated = param.eval(scope);
+
+		if (evaluated.is_boolean() && !evaluated.get<bool>()) {
+			return false;
+		} else if (evaluated.is_null()) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+json
+S_or(const vector<Node>& params, Scope* scope) {
+	for (const Node& param : params) {
+		json evaluated = param.eval(scope);
+
+		if (evaluated.is_boolean() && evaluated.get<bool>()) {
+			return true;
+		} else if (!evaluated.is_boolean() && !evaluated.is_null()) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+json
+S_not(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return true;
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (evaluated.is_boolean()) {
+		return !evaluated.get<bool>();
+	} else if (evaluated.is_null()) {
+		return true;
+	}
+
+	return false;
+}
+
+json
+S_lowercase(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_string()) {
+		return json();
+	}
+
+	string str = evaluated.get<string>();
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	return str;
+}
+
+json
+S_uppercase(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_string()) {
+		return json();
+	}
+
+	string str = evaluated.get<string>();
+	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+	return str;
+}
+
+json
+S_trim(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_string()) {
+		return json();
+	}
+
+	string str = evaluated.get<string>();
+	size_t start = str.find_first_not_of(" \t\n\r\f\v");
+	if (start == string::npos) {
+		return "";
+	}
+	size_t end = str.find_last_not_of(" \t\n\r\f\v");
+	return str.substr(start, end - start + 1);
+}
+
+json
+S_cons(const vector<Node>& params, Scope* scope) {
+	if (params.size() < 2) {
+		return json();
+	}
+
+	Node head_node = params[0];
+	json head = head_node.eval(scope);
+
+	Node tail_node = params[1];
+	json tail = tail_node.eval(scope);
+
+	if (!tail.is_array()) {
+		return json();
+	}
+
+	json result = json::parse("[]");
+	result.push_back(head);
+	for (i32 i = 0; i < tail.size(); i++) {
+		result.push_back(tail.at(i));
+	}
+
+	return result;
+}
+
+json
+S_car(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_array() || evaluated.empty()) {
+		return json();
+	}
+
+	return evaluated.at(0);
+}
+
+json
+S_cdr(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_array() || evaluated.size() <= 1) {
+		return json::parse("[]");
+	}
+
+	json result = json::parse("[]");
+	for (i32 i = 1; i < evaluated.size(); i++) {
+		result.push_back(evaluated.at(i));
+	}
+
+	return result;
+}
+
+json
+S_nth_helper(const vector<Node>& params, Scope* scope, i32 index) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_array() || evaluated.size() <= index) {
+		return json();
+	}
+
+	return evaluated.at(index);
+}
+
+json S_first(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 0); }
+json S_second(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 1); }
+json S_third(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 2); }
+json S_fourth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 3); }
+json S_fifth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 4); }
+json S_sixth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 5); }
+json S_seventh(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 6); }
+json S_eighth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 7); }
+json S_ninth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 8); }
+json S_tenth(const vector<Node>& params, Scope* scope) { return S_nth_helper(params, scope, 9); }
+
+json
+S_length(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (evaluated.is_array()) {
+		return (i32)evaluated.size();
+	} else if (evaluated.is_string()) {
+		return (i32)evaluated.get<string>().size();
+	}
+
+	return json();
+}
+
+json
+S_append(const vector<Node>& params, Scope* scope) {
+	json result = json::parse("[]");
+
+	for (const Node& param : params) {
+		json evaluated = param.eval(scope);
+
+		if (evaluated.is_array()) {
+			for (i32 i = 0; i < evaluated.size(); i++) {
+				result.push_back(evaluated.at(i));
+			}
+		}
+	}
+
+	return result;
+}
+
+json
+S_concat(const vector<Node>& params, Scope* scope) {
+	string result = "";
+
+	for (const Node& param : params) {
+		json evaluated = param.eval(scope);
+
+		if (evaluated.is_string()) {
+			result += evaluated.get<string>();
+		}
+	}
+
+	return result;
+}
+
+json
+S_join(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node list_node = params[0];
+	json list = list_node.eval(scope);
+
+	if (!list.is_array()) {
+		return json();
+	}
+
+	string separator = "";
+	if (params.size() >= 2) {
+		Node sep_node = params[1];
+		json sep = sep_node.eval(scope);
+		if (sep.is_string()) {
+			separator = sep.get<string>();
+		}
+	}
+
+	string result = "";
+	for (i32 i = 0; i < list.size(); i++) {
+		if (i > 0) {
+			result += separator;
+		}
+		json item = list.at(i);
+		if (item.is_string()) {
+			result += item.get<string>();
+		}
+	}
+
+	return result;
+}
+
+json
+S_mod(const vector<Node>& params, Scope* scope) {
+	if (params.size() < 2) {
+		return json();
+	}
+
+	Node left_node = params[0];
+	json left = left_node.eval(scope);
+
+	Node right_node = params[1];
+	json right = right_node.eval(scope);
+
+	if (!left.is_number() || !right.is_number()) {
+		return json();
+	}
+
+	f64 divisor = right.get<f64>();
+	if (divisor == 0) {
+		return json();
+	}
+
+	return std::fmod(left.get<f64>(), divisor);
+}
+
+json
+S_split(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_string()) {
+		return json();
+	}
+
+	string str = evaluated.get<string>();
+	string delimiter = "";
+	if (params.size() >= 2) {
+		Node delim_node = params[1];
+		json delim = delim_node.eval(scope);
+		if (delim.is_string()) {
+			delimiter = delim.get<string>();
+		}
+	}
+
+	json result = json::parse("[]");
+
+	if (delimiter.empty()) {
+		for (i32 i = 0; i < str.size(); i++) {
+			result.push_back(string(1, str[i]));
+		}
+	} else {
+		size_t pos = 0;
+		size_t found;
+		while ((found = str.find(delimiter, pos)) != string::npos) {
+			result.push_back(str.substr(pos, found - pos));
+			pos = found + delimiter.size();
+		}
+		result.push_back(str.substr(pos));
+	}
+
+	return result;
+}
+
+json
+S_contains(const vector<Node>& params, Scope* scope) {
+	if (params.size() < 2) {
+		return false;
+	}
+
+	Node haystack_node = params[0];
+	json haystack = haystack_node.eval(scope);
+
+	Node needle_node = params[1];
+	json needle = needle_node.eval(scope);
+
+	if (haystack.is_array()) {
+		for (i32 i = 0; i < haystack.size(); i++) {
+			if (haystack.at(i) == needle) {
+				return true;
+			}
+		}
+		return false;
+	} else if (haystack.is_string() && needle.is_string()) {
+		return haystack.get<string>().find(needle.get<string>()) != string::npos;
+	}
+
+	return false;
+}
+
+json
+S_abs(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (evaluated.is_number()) {
+		return std::abs(evaluated.get<f64>());
+	}
+
+	return json();
+}
+
+json
+S_reverse(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (evaluated.is_array()) {
+		json result = json::parse("[]");
+		for (i32 i = evaluated.size() - 1; i >= 0; i--) {
+			result.push_back(evaluated.at(i));
+		}
+		return result;
+	} else if (evaluated.is_string()) {
+		string str = evaluated.get<string>();
+		std::reverse(str.begin(), str.end());
+		return str;
+	}
+
+	return json();
+}
+
+json
+S_last(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_array() || evaluated.empty()) {
+		return json();
+	}
+
+	return evaluated.at(evaluated.size() - 1);
+}
+
+json
+S_compact(const vector<Node>& params, Scope* scope) {
+	if (params.empty()) {
+		return json();
+	}
+
+	Node v = params[0];
+	json evaluated = v.eval(scope);
+
+	if (!evaluated.is_array()) {
+		return json();
+	}
+
+	json result = json::parse("[]");
+	for (i32 i = 0; i < evaluated.size(); i++) {
+		if (!evaluated.at(i).is_null()) {
+			result.push_back(evaluated.at(i));
+		}
+	}
+
+	return result;
+}
+
+json
 S_print(const vector<Node>& params, Scope* scope) {
 	if (params.empty()) {
 		return json();
@@ -664,6 +1116,7 @@ std::unordered_map<string, ScrapFunction> operations = {
 	{"if", &S_if},
 	{"let", &S_let},
 	{"array", &S_array},
+	{"list", &S_array},
 	{"=", &S_equal},
 	{"!=", &S_nequal},
 	{">", &S_gt},
@@ -684,7 +1137,37 @@ std::unordered_map<string, ScrapFunction> operations = {
 	{"min", &S_min},
 	{"max", &S_max},
 	{"nth", &S_nth},
+	{"length", &S_length},
+	{"append", &S_append},
+	{"concat", &S_concat},
+	{"join", &S_join},
+	{"mod", &S_mod},
 	{"print", &S_print},
+	{"first", &S_first},
+	{"second", &S_second},
+	{"third", &S_third},
+	{"fourth", &S_fourth},
+	{"fifth", &S_fifth},
+	{"sixth", &S_sixth},
+	{"seventh", &S_seventh},
+	{"eighth", &S_eighth},
+	{"ninth", &S_ninth},
+	{"tenth", &S_tenth},
+	{"cons", &S_cons},
+	{"car", &S_car},
+	{"cdr", &S_cdr},
+	{"lowercase", &S_lowercase},
+	{"uppercase", &S_uppercase},
+	{"trim", &S_trim},
+	{"not", &S_not},
+	{"and", &S_and},
+	{"or", &S_or},
+	{"split", &S_split},
+	{"contains", &S_contains},
+	{"abs", &S_abs},
+	{"reverse", &S_reverse},
+	{"last", &S_last},
+	{"compact", &S_compact},
 };
 
 ScrapFunction get_scrap_function(const string& key) {
